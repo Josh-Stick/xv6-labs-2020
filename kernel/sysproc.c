ef2dee8b04ae394bfab2bf5d5a47dab60c3310d6
ning <2716161418@qq.com>
2025-03-31 09:12:34 +0800

    feat: Lab5_work1 Eliminate allocation from sbrk() 改动系统调用sys_sbrk，不再调用分配内存的growproc，仅让进程的sz加上新增的内存,并返回旧的sz

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -41,15 +41,21 @@ sys_wait(void)
 uint64
 sys_sbrk(void)
 {
-  int addr;
+  int addr; //保存旧的sz
   int n;
 
   if(argint(0, &n) < 0)
     return -1;
   addr = myproc()->sz;
-  if(growproc(n) < 0)
-    return -1;
-  return addr;
+  // if(growproc(n) < 0)
+  //   return -1;
+  if (n > 0) {
+    myproc()->sz += n;
+  }
+  else if (myproc()->sz + n > 0) {
+    myproc()->sz = uvmdealloc(myproc()->pagetable, myproc()->sz, myproc()->sz + n);
+  }
+  return addr;   //返回的也是旧的sz
 }
 
 uint64
